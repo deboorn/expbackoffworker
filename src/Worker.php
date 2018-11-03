4ae1c6d76dbbc0fa137bf2576369bfa013734ca0
Eric Tendian <erictendian@gmail.com>
2018-11-02 21:26:29 -0500

    Updating documentation to match Laravel 5.3+'s rename of expire to retry_after

--- src/Worker.php
+++ src/Worker.php
@@ -1,6 +1,5 @@
 <?php namespace ExpBackoffWorker;
 
-use Exception;
 use Illuminate\Queue\Worker as IlluminateQueueWorker;
 use Illuminate\Queue\WorkerOptions;
 
@@ -19,14 +18,7 @@ class Worker extends IlluminateQueueWorker
      */
     protected function handleJobException($connectionName, $job, WorkerOptions $options, $e)
     {
-        // Add exponential delay based on the job attempts and the provided delay seconds.
-        // The delay will default to 30 seconds by default or when delay is set to zero.
-        // A max delay of 2 hours will be used when exponential delay exceeds 2 hours
-        // Example of delay in seconds: 30, 60, 90, 180, ... 7200
-        $options->delay = $options->delay ?: 30;
-        $options->delay = $job->attempts() > 1 ? (pow(2, $job->attempts() - 2) * $options->delay) : 0;
-        $maxDelay = 60 * 60 * 2;
-        if ($options->delay > $maxDelay) $options->delay = $maxDelay;
+        $options->delay = (new GetDelay)($options->delay, $job->attempts());
 
         parent::handleJobException($connectionName, $job, $options, $e);
     }
